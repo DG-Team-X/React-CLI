2380e04f5bffb7df45e36c2baafa130fd89e954d
Pako <kirill.mitin34@gmail.com>
2021-05-26 20:20:27 +0300

    fixes from exports

--- src/generators/templates/exportsTemplate.js
+++ src/generators/templates/exportsTemplate.js
@@ -1,5 +1,5 @@
 export default (components) => {
 return `
-${components.map(component => `export {default as ${component}} from './${component}'`).join("\n")}
+${components.map(component => `export {default as ${component}} from './${component}/${component}'`).join("\n")}
 `
 }
