157727fabe19ba59a0cf9e03c1704c3eea2fcadf
Alex Safr <a@slto.ru>
2021-06-23 23:13:10 +0300

    build 1.1.0 v

--- build/generators/templates/exportsTemplate.js
+++ build/generators/templates/exportsTemplate.js
@@ -7,7 +7,7 @@ exports["default"] = void 0;
 
 var _default = function _default(components) {
   return "\n".concat(components.map(function (component) {
-    return "export {default as ".concat(component, "} from './").concat(component, "'");
+    return "export {default as ".concat(component, "} from './").concat(component, "/").concat(component, "'");
   }).join("\n"), "\n");
 };
 
